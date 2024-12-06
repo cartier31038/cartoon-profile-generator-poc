e283d11bc2714d611e535805e1d9999a627806fe
Nitirat Saengchaiarun <nitirat@praneat.com>
2024-12-06 13:09:52 +0700

    feat: 🎸load_prompt - enhance one shot prompt (more appearance detail) feat: 🎸 add generate_image - request XHR image generator to SD API

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
 import streamlit as st
-from PIL import Image
-from utils import load_prompt, prompt_with_llm
+import io
+import base64
 import logging
+
+from PIL import Image
+from utils import load_prompt, prompt_with_llm, generate_image
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -12,7 +15,7 @@ def main():
     st.title("Cartoon Profile Generator ( ^▽^)σ")
 
     user_question = st.text_input("Input prompt", key="input")
-
+    
     st.sidebar.title("Profile Image")
 
     uploaded_file = st.sidebar.file_uploader(
@@ -30,21 +33,27 @@ def main():
             st.error(f"Error opening the image: {str(e)}")
             logging.error(f"Error opening the image: {str(e)}")
 
-    prompt = load_prompt()
-
-    if st.button("Generate"):
+    caption = None
+    if st.button("Generate Profile"):
         if image is None:
             st.warning("Please upload an image before generating.")
             return
 
-        with st.spinner("Cartoon profile generating..."):
+        with st.spinner("Profile generating..."):
             try:
-                response = prompt_with_llm(input_question=user_question, prompt=prompt, image=image)
-                st.subheader("Response:")
-                st.write(response)
+                prompt = load_prompt()
+                caption = prompt_with_llm(input_question=user_question, prompt=prompt, image=image)
+                st.subheader("Caption:")
+                st.write(caption)
+                
+                result = generate_image(caption)
+                image_bytes = base64.b64decode(result['images'][0])
+                final_image = Image.open(io.BytesIO(image_bytes))
+                st.image(final_image, caption="Result image", use_column_width=True)
+    
             except Exception as e:
                 st.error(f"An error occurred during generation: {str(e)}")
-                logging.error(f"Error in prompt_with_llm: {str(e)}", exc_info=True)
-
+                logging.error(f"An error occurred during generation: {str(e)}", exc_info=True)
+    
 if __name__ == "__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,10 @@
 import os
 import google.generativeai as genai
-from dotenv import load_dotenv
+import logging
+import requests
+import json
 
+from dotenv import load_dotenv
 load_dotenv()
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
@@ -9,8 +12,10 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 def load_prompt():
     input_prompt = """
-                   You are tool for image captioning and generate prompt for Stable Diffusion .
-                   Example prompt: Headshot of a young Southeast Asian man (weight: 1.4), friendly smile (weight: 1.2), short brown hair styled upwards (weight: 1.3), wearing a dark gray t-shirt with 'Los White Sox' logo and emblem (weight: 1.5), small hoop earrings (weight: 1.1), against a plain off-white background (weight: 1.2). Clear skin (weight: 1.1), warm complexion (weight: 1.2), direct eye contact (weight: 1.3), shoulders visible (weight: 1.1), soft indoor lighting (weight: 1.0), casual and approachable demeanor (weight: 1.2), centered composition (weight: 1.1).
+                   You are tool for image captioning and generate prompt for Stable Diffusion AnyLoRA.
+                   
+                   Example prompt: (young Asian woman:1.5), (25 years old:1.3), (oval face:1.3), (clear skin:1.3), (warm complexion:1.2), (bright almond-shaped eyes:1.4), (natural makeup:1.2), (defined eyebrows:1.2), (friendly smile:1.3), (straight white teeth:1.2), (small nose:1.2), (black hair pulled back:1.4), (neat bun hairstyle:1.3), (slender neck:1.2), (light beige hooded t-shirt:1.3), (small yellow pin on shirt:1.2), (athletic build:1.2), (good posture:1.3), (standing in an office:1.3), (small tattoo on left arm:1.1), (professional appearance:1.3)
+                   
                    Please generate prompt for Stable Diffusion including weight on each prominent point and response just only prompt text.
                    """
     return input_prompt
@@ -19,7 +24,43 @@ def prompt_with_llm(input_question, prompt, image):
     model = genai.GenerativeModel("gemini-1.5-pro-latest")
     response = model.generate_content([input_question, prompt, image])
     return f"""
-            (best quality:1.1), solo, upper body, looking at viewer, (solid white background:1.1),
-            {response.text} 
-            \<lora:last:0.9\>
+            (best quality:1.5), (solo:1.4), (upper body:1.4), (looking at viewer:1.4),
+            {response.text}
+            <lora:last:0.9>
             """
+            
+def generate_image(prompt):
+    
+    url = f"{os.getenv('SD_API_URL')}/sdapi/v1/txt2img"
+
+    headers = {
+        "Content-Type": "application/json",
+    }
+
+    logging.info(f"generate_image.prompt: {prompt}", exc_info=True)
+    
+    data = {
+        "prompt": prompt,
+        "negative_prompt": "lowres, blurry, worst quality, low quality, normal quality, many people, bad anatomy, bad hands, missing fingers, error, text, username, extra digit, fewer digits, signature, watermark, cropped, jpeg artifacts",
+        "batch_size": 1,
+        "n_iter": 1,
+        "steps": 30,
+        "cfg_scale": 7,
+        "width": 512,
+        "height": 512,
+        "restore_faces": False,
+        "tiling": False,
+        "seed": -1,
+        "subseed": -1,
+        "subseed_strength": 0,
+        "sampler_index": "Euler a",
+        "save_images": False,
+        "send_images": True
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    result = json.loads(response.text)
+    logging.info(f"generate_image.code: {str(response.status_code)}", exc_info=True)
+    # logging.info(f"generate_image.text: {str(response.text)}", exc_info=True)
+    
+    return result
